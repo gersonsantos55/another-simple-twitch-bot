926a3ec9abd03c9aed9010e3c7f182c41b25d1d5
gersonsantos55 <gersonsantos55@gmail.com>
2022-12-03 17:26:55 -0300

    Removed milliliters()

--- src/modules/random.ts
+++ src/modules/random.ts
@@ -1,7 +1,12 @@
 export function randomNumberBetween(start: number, end: number): number {
-  return Math.floor(Math.random() * (end - start) + start);
-}
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    return 0;
+  }
 
-export function milliliters(): number {
-  return randomNumberBetween(50, 1000);
+  // Ensure that start is less than or equal to end
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+
+  return Math.ceil(Math.random() * (end - start) + start);
 }
