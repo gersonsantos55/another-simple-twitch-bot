f62e3ce0072a4dd0baf5c3cc8d4cbdcad76a3681
gersonsantos55 <gersonsantos55@gmail.com>
2022-12-03 11:41:15 -0300

    Add feature to delete messages from new accounts

--- src/modules/check-user-message.ts
+++ src/modules/check-user-message.ts
@@ -4,24 +4,33 @@ import { client } from "../index"
 
 const BOT_USERNAMES: string[] = [];
 
+const BLOCKED_WORDS: string[] = [];
+
 export function checkUserMessage(channel: string, userstate: tmi.ChatUserstate, message: string): void {
 
   if (BOT_USERNAMES.includes(userstate.username as string)) {
     return;
   }
 
+  // To better handle
   message = message.toLowerCase();
-  client.say(channel, `funciona`);
-  // shouldSendMessage = [].some((blockedWord) =>
-  //   message.includes(blockedWord.toLowerCase())
-  // );
-  // if (shouldSendMessage) {
-  //   // tell user
-  //   client.say(
-  //     channel,
-  //     `@${userstate.username}, sorry!  You message was deleted.`
-  //   );
-  //   // delete message
-  //   client.deletemessage(channel, userstate.id);
-  // }
+  const messageUUID = userstate.id as string;
+
+  // Check if the user's account was created in less than 1 day
+  if (Date.now() - userstate.createdAt < 86400000) {
+    // Delete the message
+    client.deletemessage(channel, messageUUID);
+
+    // Send a message explaining why the message was deleted
+    client.say(channel, `@${userstate.username}, your message was deleted because your account was created less than 1 day ago.`);
+  }
+
+  // Check if the message has some blocked word
+  if (BLOCKED_WORDS.some(word => message.includes(word.toLowerCase()))) {
+    // Send a message explaining why the message was deleted
+    client.say(channel, `@${userstate.username}, sorry! You message was deleted because have some forbidden word.`);
+    // delete message
+    client.deletemessage(channel, messageUUID);
+  }
+
 }
