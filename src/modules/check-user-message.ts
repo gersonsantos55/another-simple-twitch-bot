2a1e0aae1e9b9b1cb13eba53e3b102aa7ff02c0a
gersonsantos55 <gersonsantos55@gmail.com>
2022-12-03 15:43:21 -0300

    Activate tea command

--- src/modules/check-user-message.ts
+++ src/modules/check-user-message.ts
@@ -1,36 +1,39 @@
 import tmi from "tmi.js";
 
 import { client } from "../index"
+import { getTeaMessage, hasTeaCommand } from "./tea";
 
 const BOT_USERNAMES: string[] = [];
 
 const BLOCKED_WORDS: string[] = [];
 
-export function checkUserMessage(channel: string, userstate: tmi.ChatUserstate, message: string): void {
+export function checkUserMessage(
+  channel: string,
+  userstate: tmi.ChatUserstate,
+  message: string
+): void {
+  if (
+    typeof channel === 'string'
+    && typeof userstate === 'object'
+    && userstate.hasOwnProperty('username')
+    && typeof userstate.username === 'string'
+    && userstate.hasOwnProperty('id')
+    && typeof userstate.id === 'string'
+  ) {
 
-  if (BOT_USERNAMES.includes(userstate.username as string)) {
-    return;
+    if (BOT_USERNAMES.includes(userstate.username)) {
+      return;
+    }
+
+    message = message.toLowerCase();
+    if (BLOCKED_WORDS.some(word => message.includes(word.toLowerCase()))) {
+      client.deletemessage(channel, userstate.id);
+      client.say(channel, `@${userstate.username}, sorry! You message was deleted because have some forbidden word.`);
+    }
+
+    if (hasTeaCommand(message)) {
+      client.say(channel, getTeaMessage(userstate.username));
+    }
+
   }
-
-  // To better handle
-  message = message.toLowerCase();
-  const messageUUID = userstate.id as string;
-
-  // Check if the user's account was created in less than 1 day
-  if (Date.now() - userstate.createdAt < 86400000) {
-    // Delete the message
-    client.deletemessage(channel, messageUUID);
-
-    // Send a message explaining why the message was deleted
-    client.say(channel, `@${userstate.username}, your message was deleted because your account was created less than 1 day ago.`);
-  }
-
-  // Check if the message has some blocked word
-  if (BLOCKED_WORDS.some(word => message.includes(word.toLowerCase()))) {
-    // Send a message explaining why the message was deleted
-    client.say(channel, `@${userstate.username}, sorry! You message was deleted because have some forbidden word.`);
-    // delete message
-    client.deletemessage(channel, messageUUID);
-  }
-
 }
